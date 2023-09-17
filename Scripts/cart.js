6c7a7cfa8dfe6b133ccf8e85f0a30c5c306aab9c
Narayan Das <workmail.narayan@gmail.com>
2023-09-17 16:46:01 +0530

    added almost all features

--- Scripts/cart.js
+++ Scripts/cart.js
@@ -1,4 +1,20 @@
+fetch("data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    let randomData = [];
+    for (let i = 0; i < 4; i++) {
+      let index = Math.floor(Math.random() * data.length);
+      randomData.push(data[index]);
+    }
+    localStorage.setItem("randomData", JSON.stringify(randomData));
+  })
+  .catch((error) => console.error("Error:", error));
+
+let randomData = JSON.parse(localStorage.getItem("randomData")) || [];
+
 var cartData = JSON.parse(localStorage.getItem("cartData")) || [];
+let allProductSubtotal = 0;
+let discount = 0;
 
 function renderProducts(data) {
   data.forEach(function (element) {
@@ -34,6 +50,7 @@ function renderProducts(data) {
     itemPriceWrapper.appendChild(itemPrice);
 
     let quantity = 1;
+    let productSubtotal = element.price * quantity;
     let quantitySelector = document.createElement("div");
     quantitySelector.id = "quantitySelector";
 
@@ -42,40 +59,41 @@ function renderProducts(data) {
     quantityDecrement.innerHTML = '<i class="fa-solid fa-minus"></i>';
     quantitySelector.appendChild(quantityDecrement);
 
+    quantityDecrement.addEventListener("click", function () {
+      if (quantity > 1) {
+        quantity--;
+        productSubtotal = quantity * element.price;
+        allProductSubtotal -= Number(element.price);
+        quantitySpan.textContent = quantity;
+        subtotalPrice.textContent = `$${productSubtotal.toFixed(2)}`;
+        updateSummery();
+      }
+    });
+
     let quantitySpan = document.createElement("span");
     quantitySpan.textContent = quantity;
     quantitySelector.appendChild(quantitySpan);
 
     let quantityIncrement = document.createElement("button");
-    quantityIncrement.id = "quantityDecrement";
+    quantityIncrement.id = "quantityIncrement";
     quantityIncrement.innerHTML = '<i class="fa-solid fa-plus"></i>';
     quantitySelector.appendChild(quantityIncrement);
+
+    quantityIncrement.addEventListener("click", function () {
+      quantity++;
+      productSubtotal = quantity * element.price;
+      allProductSubtotal += Number(element.price); // add price of one product
+      quantitySpan.textContent = quantity;
+      subtotalPrice.textContent = `$${productSubtotal.toFixed(2)}`;
+      updateSummery();
+    });
 
     let subtotalWrapper = document.createElement("div");
     subtotalWrapper.id = "subtotalWrapper";
 
     let subtotalPrice = document.createElement("p");
-    subtotalPrice.textContent = `$${(quantity * Number(element.price)).toFixed(
-      2
-    )}`;
+    subtotalPrice.textContent = `$${productSubtotal.toFixed(2)}`;
 
-    quantityDecrement.addEventListener("click", function () {
-      if (quantity > 1) {
-        quantity--;
-        quantitySpan.innerHTML = quantity;
-        subtotalPrice.textContent = `$${(
-          quantity * Number(element.price)
-        ).toFixed(2)}`;
-      }
-    });
-
-    quantityIncrement.addEventListener("click", function () {
-      quantity++;
-      quantitySpan.innerHTML = quantity;
-      subtotalPrice.textContent = `$${(
-        quantity * Number(element.price)
-      ).toFixed(2)}`;
-    });
     subtotalWrapper.appendChild(subtotalPrice);
 
     let removeIconWrapper = document.createElement("div");
@@ -94,7 +112,10 @@ function renderProducts(data) {
     let parent = document.querySelector("#cartItemShowCaseWrapper");
     parent.appendChild(cartProductSingleCard);
     renderWishlistSection();
+    allProductSubtotal += productSubtotal;
   });
+  console.log(allProductSubtotal);
+  updateSummery();
 }
 
 function renderWishlistSection() {
@@ -129,4 +150,109 @@ function renderWishlistSection() {
   parent.appendChild(cartWishlistDiv);
 }
 
+// Bottom product showcase
+
+function renderProductCards(data) {
+  var parentDiv = document.querySelector("#upsellProductShowcase");
+  parentDiv.innerHTML = "";
+  data.forEach(function (element, index) {
+    var image_url = element.image_urls[0];
+    var name = element.name;
+    var price = element.price;
+
+    var cardDiv = document.createElement("div");
+    cardDiv.setAttribute("class", "productCardDiv");
+    var imageAndWishListDiv = document.createElement("div");
+    imageAndWishListDiv.setAttribute("id", "imageAndWishListDiv");
+    var imageDiv = document.createElement("div");
+    imageDiv.setAttribute("id", "imageDiv");
+
+    var image = document.createElement("img");
+
+    var productDetailsDiv = document.createElement("div");
+    productDetailsDiv.setAttribute("id", "productDetailsDiv");
+
+    var proName = document.createElement("h3");
+    proName.innerHTML = name;
+
+    // Stars
+    var ratingContainer = document.createElement("div");
+    ratingContainer.setAttribute("id", "ratingContainer");
+
+    var ratingStars = document.createElement("span");
+    ratingStars.setAttribute("id", "ratingStars");
+
+    var noOfRatings = document.createElement("span");
+    noOfRatings.setAttribute("id", "noOfRatings");
+    noOfRatings.innerHTML = `(${element.reviews.total_reviews})`;
+
+    var ratingStars = document.createElement("img");
+    ratingStars.setAttribute("id", "ratingStars");
+    ratingStars.src = "Media/stars.png";
+
+    ratingContainer.append(ratingStars, noOfRatings);
+
+    // Price element
+
+    var priceElement = document.createElement("p");
+    priceElement.setAttribute("id", "priceElement");
+    priceElement.innerHTML = `$${price}`;
+
+    productDetailsDiv.append(proName, ratingContainer, priceElement);
+
+    image.src = image_url;
+    imageDiv.append(image);
+    imageAndWishListDiv.append(imageDiv);
+    var addToCartButton = document.createElement("button");
+
+    addToCartButton.innerHTML = "QUICK BUY";
+    addToCartButton.setAttribute("id", "addToCartButton");
+
+    cardDiv.append(imageAndWishListDiv, productDetailsDiv, addToCartButton);
+
+    image.addEventListener("mouseover", function () {
+      if (element.image_urls.length > 1) {
+        image.style.transition = "all 0.5s ease";
+        image.src = element.image_urls[1];
+      }
+    });
+
+    image.addEventListener("mouseout", function () {
+      image.style.transition = "all 0.5s ease";
+      image.src = element.image_urls[0];
+    });
+
+    let clickedCart = false;
+    addToCartButton.addEventListener("click", function () {
+      if (!clickedCart) {
+        clickedCart = true;
+        cartData.push(element);
+        localStorage.setItem("cartData", JSON.stringify(cartData));
+        openPopup();
+        renderPopupData(element);
+      } else {
+        alert("Produt is alredy in the cart");
+      }
+    });
+
+    var parentDiv = document.querySelector("#upsellProductShowcase");
+    parentDiv.append(cardDiv);
+  });
+}
+
+function updateSummery() {
+  let itemCount = document.querySelector("#pricingFinalSubtotal");
+  itemCount.innerHTML = `Subtotal (${cartData.length} Items)`;
+
+  let discountElement = document.querySelector("#discountPrice");
+  discountElement.innerHTML = `-$${discount.toFixed(2)}`;
+
+  let cartSummerySubtotal = document.querySelector("#cartSummerySubtotal");
+  cartSummerySubtotal.innerHTML = `$${allProductSubtotal.toFixed(2)}`;
+
+  let totalCartPrice = document.querySelector("#totalCartPrice");
+  totalCartPrice.innerHTML = `$${(allProductSubtotal - discount).toFixed(2)}`;
+}
+updateSummery();
+renderProductCards(randomData);
 renderProducts(cartData);
