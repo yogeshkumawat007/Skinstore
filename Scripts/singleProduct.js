de52edca7afd4013363b69d99f0625c24c37cb48
Narayan Das <workmail.narayan@gmail.com>
2023-09-19 17:56:15 +0530

    made single product page responsive and added products

--- Scripts/singleProduct.js
+++ Scripts/singleProduct.js
@@ -1,3 +1,16 @@
+fetch("data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    let randomData = [];
+    for (let i = 0; i < 4; i++) {
+      let index = Math.floor(Math.random() * data.length);
+      randomData.push(data[index]);
+    }
+    localStorage.setItem("randomData", JSON.stringify(randomData));
+  })
+  .catch((error) => console.error("Error:", error));
+
+let randomData = JSON.parse(localStorage.getItem("randomData")) || [];
 let singleProArr = [];
 var cartData = JSON.parse(localStorage.getItem("cartData")) || [];
 let wishlistData = JSON.parse(localStorage.getItem("wishlistData")) || [];
@@ -103,6 +116,97 @@ window.onload = function () {
     localStorage.setItem("cartData", JSON.stringify(cartData));
   }
 };
+
+// Bottom product showcase
+function renderProductCards(data) {
+  var parentDiv = document.querySelector("#upsellProductShowcase");
+  parentDiv.innerHTML = "";
+  data.forEach(function (element, index) {
+    var image_url = element.image_urls[0];
+    var name = element.name;
+    var price = element.price;
+
+    var cardDiv = document.createElement("div");
+    cardDiv.setAttribute("class", "productCardDiv");
+    var imageAndWishListDiv = document.createElement("div");
+    imageAndWishListDiv.setAttribute("id", "imageAndWishListDiv");
+    var imageDiv = document.createElement("div");
+    imageDiv.setAttribute("id", "imageDiv");
+
+    var image = document.createElement("img");
+
+    var productDetailsDiv = document.createElement("div");
+    productDetailsDiv.setAttribute("id", "productDetailsDiv");
+
+    var proName = document.createElement("h3");
+    proName.innerHTML = name;
+
+    // Stars
+    var ratingContainer = document.createElement("div");
+    ratingContainer.setAttribute("id", "ratingContainer");
+
+    var ratingStars = document.createElement("span");
+    ratingStars.setAttribute("id", "ratingStars");
+
+    var noOfRatings = document.createElement("span");
+    noOfRatings.setAttribute("id", "noOfRatings");
+    noOfRatings.innerHTML = `(${element.reviews.total_reviews})`;
+
+    var ratingStars = document.createElement("img");
+    ratingStars.setAttribute("id", "ratingStars");
+    ratingStars.src = "Media/stars.png";
+
+    ratingContainer.append(ratingStars, noOfRatings);
+
+    // Price element
+
+    var priceElement = document.createElement("p");
+    priceElement.setAttribute("id", "priceElement");
+    priceElement.innerHTML = `$${price}`;
+
+    productDetailsDiv.append(proName, ratingContainer, priceElement);
+
+    image.src = image_url;
+    imageDiv.append(image);
+    imageAndWishListDiv.append(imageDiv);
+    var addToCartButton = document.createElement("button");
+
+    addToCartButton.innerHTML = "QUICK BUY";
+    addToCartButton.setAttribute("id", "addToCartButton");
+
+    cardDiv.append(imageAndWishListDiv, productDetailsDiv, addToCartButton);
+
+    image.addEventListener("mouseover", function () {
+      if (element.image_urls.length > 1) {
+        image.style.transition = "all 0.5s ease";
+        image.src = element.image_urls[1];
+      }
+    });
+
+    image.addEventListener("mouseout", function () {
+      image.style.transition = "all 0.5s ease";
+      image.src = element.image_urls[0];
+    });
+
+    let clickedCart = false;
+    addToCartButton.addEventListener("click", function () {
+      if (!clickedCart) {
+        clickedCart = true;
+        cartData.push(element);
+        localStorage.setItem("cartData", JSON.stringify(cartData));
+        openPopup();
+        renderPopupData(element);
+      } else {
+        alert("Produt is alredy in the cart");
+      }
+    });
+
+    var parentDiv = document.querySelector("#upsellProductShowcase");
+    parentDiv.append(cardDiv);
+  });
+}
+
+renderProductCards(randomData);
 
 $(function () {
   $("#footer").load("footer.html");
